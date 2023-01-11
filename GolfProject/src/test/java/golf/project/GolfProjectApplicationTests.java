6f4002376c2c7958ee47b8922bd190546c26a293
kimkyungchan <84487848+kimkyungchan@users.noreply.github.com>
2023-01-11 20:05:18 +0900

    골프 프로젝트
    초기 골프 프로젝트

---
+++ GolfProject/src/test/java/golf/project/GolfProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package golf.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolfProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
